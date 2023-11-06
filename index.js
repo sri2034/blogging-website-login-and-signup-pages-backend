aa2f35ff6db24cf6088f745950a9764ce4b761fd
sri2034 <143617953+sri2034@users.noreply.github.com>
2023-11-06 22:48:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ db.on("error",()=>console.log("Error occurred"));
 
 const userSchema = new mongoose.Schema({
     name: String,
+    username: String,
     email: String,
-    password: String
+    phno: String,
+    password: String,
+    dob: Date
 },{
     collection: "User"
 });
@@ -23,13 +26,13 @@ app.use(cors());
 const User = new mongoose.model("User",userSchema)
 
 app.post("/login", (req, res)=> {
-    const { email, password} = req.body
-    User.findOne({ email: email}, (err, user) => {
+    const { username, password} = req.body
+    User.findOne({ username: username}, (err, user) => {
         if(user){
             if(password === user.password ) {
                 res.send({message: "Login Successfull", user: user})
             } else {
-                res.send({ message: "Password didn't match"})
+                res.send({ message: "Incorrect Password"})
             }
         } else {
             res.send({message: "User not registered"})
@@ -38,27 +41,44 @@ app.post("/login", (req, res)=> {
 }) 
 
 app.post("/register", (req, res) => {
-    const {name, email, password} = (req.body)
-    User.findOne({email: email},(err, user) => {
-        if(user){
-            res.send({message: "User already registered"})
-        }else {
+    const { name, username, email, phno, password, dob } = req.body;
+  
+    // Check if the email already exists
+    User.findOne({ email: email }, (err, existingEmailUser) => {
+      if (err) {
+        res.status(500).json({ message: "Server error" });
+      } else if (existingEmailUser) {
+        res.status(400).json({ message: "Email already registered" });
+      } else {
+        // Check if the username already exists
+        User.findOne({ username: username }, (err, existingUsernameUser) => {
+          if (err) {
+            res.status(500).json({ message: "Server error" });
+          } else if (existingUsernameUser) {
+            res.status(400).json({ message: "Username already exists" });
+          } else {
+            // If both email and username are unique, create a new user
             const user = new User({
-                name,
-                email,
-                password
-            })
-            user.save(err => {
-                if(err) {
-                    res.send(err)
-                } else {
-                    res.send({message: "Successfully Registered,Please login now."})
-                }
-            })
-        }
-    })
-    
-})
+              name,
+              username,
+              email,
+              phno,
+              password,
+              dob
+            });
+  
+            user.save((err) => {
+              if (err) {
+                res.status(500).json({ message: "Registration failed" });
+              } else {
+                res.status(200).json({ message: "Successfully Registered. Please login now." });
+              }
+            });
+          }
+        });
+      }
+    });
+});
 
 app.listen(4000,() => {
     console.log("Server started at 4000")
